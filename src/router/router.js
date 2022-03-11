9e7879c5b6f694263f54301169f31632671d1784
sevikpetrov <petrovmal@gmail.com>
2022-03-11 15:53:29 +0300

    dynamic router

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import Main from "@/pages/Main"
 import PostPage from "@/pages/PostPage"
 import About from "@/pages/About"
+import PostIdPage from "@/pages/PostIdPage"
 
 import {createRouter, createWebHistory} from "vue-router"
 
@@ -16,6 +17,10 @@ const routes =[
     {
         path:'/about',
         component:About
+    },
+    {
+        path:'/posts/:id',
+        component:PostIdPage
     }
 ]
 
